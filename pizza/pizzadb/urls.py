5634c014ce1339b8f82babf223bbd246cb8ed0c1
Filip Pawlak <filip.pawlak@gmail.com>
2013-06-18 21:18:29 +0200

    urls.py

--- pizza/pizzadb/urls.py
+++ pizza/pizzadb/urls.py
@@ -13,8 +13,11 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls) ),
-	url(r'^menu/$', 'pizzadb.views.menu' ),
-        url(r'^wlasnapizza/$', 'pizzadb.views.wlasnapizza' ),
-        url(r'^dodajpizze/$', 'pizzadb.views.dodajpizze'),                       
+                           
+                       url(r'^admin/', include(admin.site.urls) ),
+                       url(r'^menu/$', 'pizzadb.views.menu' ),
+                       url(r'^wlasnapizza/$', 'pizzadb.views.wlasnapizza' ),
+                       url(r'^dodajpizze/$', 'pizzadb.views.dodajpizze'),                       
+                       url(r'^logowanie/$', 'pizzadb.views.logowanie' ),
+                       url(r'^log/$', 'pizzadb.views.log' ),
 )
