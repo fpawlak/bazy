5afebc5e608306930ff23cca7340e987d20f0079
Filip Pawlak <filip.pawlak@gmail.com>
2013-06-19 00:33:38 +0200

    edycja pizz i dodatków z menu administratora

--- pizza/pizzadb/admin.py
+++ pizza/pizzadb/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from pizzadb import models
+from pizzadb.models import Pizza, Dodatek
 
 #admin.site.register( models.Klient )
+admin.site.register(Pizza)
+admin.site.register(Dodatek)
