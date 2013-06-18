757bffe81e60add21187d90597b939f869af2b3e
Filip Pawlak <filip.pawlak@gmail.com>
2013-06-18 18:47:44 +0200

    hasło+host

--- pizza/pizza/settings.py
+++ pizza/pizza/settings.py
@@ -15,8 +15,8 @@ DATABASES = {
         'NAME': 'pizza',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'strzelec',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PASSWORD': 'tajnehaslo',
+        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
