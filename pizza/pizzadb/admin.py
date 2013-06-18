4f2748c11560f4ab0ff4fea2982430ce1d97622b
Filip Pawlak <filip.pawlak@gmail.com>
2013-06-19 00:37:57 +0200

    składniki też można edytować

--- pizza/pizzadb/admin.py
+++ pizza/pizzadb/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from pizzadb import models
-from pizzadb.models import Pizza, Dodatek
+from pizzadb.models import Pizza, Dodatek, Skladnik
 
 #admin.site.register( models.Klient )
 admin.site.register(Pizza)
 admin.site.register(Dodatek)
+admin.site.register(Skladnik)
