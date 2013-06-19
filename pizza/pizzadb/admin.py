393773b2c81464356f48a47f8f83f9cfddf9a17d
Piotr Strzelecki <piotrekwol@gmail.com>
2013-06-19 16:54:34 +0200

    w sumie to nie wiem co

--- pizza/pizzadb/admin.py
+++ pizza/pizzadb/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from pizzadb import models
-from pizzadb.models import Pizza, Dodatek, Skladnik
+from pizzadb.models import Pizza, Dodatek, Skladnik, Uzytkownik
 
 #admin.site.register( models.Klient )
 admin.site.register(Pizza)
 admin.site.register(Dodatek)
 admin.site.register(Skladnik)
+admin.site.register(Uzytkownik)
